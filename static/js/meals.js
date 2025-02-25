bfdcc32ea0f520c46cc8c5f22ad0c79166705a5e
Madison-PD <dhaueter@cityofmadison.com>
2025-02-25 17:29:22 -0600

    update layout for mobile

--- static/js/meals.js
+++ static/js/meals.js
@@ -130,7 +130,7 @@ function addMealLogEntry() {
     newRow.innerHTML = `
         <input type="date" name="log_entry_date_${i}" class="log-date" placeholder="Date">
         <input type="number" name="log_entry_rating_${i}" class="log-rating" size="4" min=0 max=10 placeholder="Rating">
-        <input type="text" name="log_entry_notes_${i}" class="log-notes" size="80" placeholder="Notes">
+        <textarea name="log_entry_notes_${i}" class="log-notes" rows="5" placeholder="Notes"></textarea>
         <button type="button" class="btn btn-sm delete-btn btn-danger" onclick="this.parentElement.remove()">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
